573976f49c6aec36ff6a08eed482f6e8b473ecd4
Tovgaard <56001124+Tovgaard@users.noreply.github.com>
2022-10-21 12:54:48 +0200

    V 0.1

---
+++ Drone v1/app.py
@@ -0,0 +1,12 @@
+import tello
+from TelloUI import TelloUI
+
+
+def main():
+    drone = tello.Tello('', 8889)  
+    vplayer = TelloUI(drone)
+    vplayer.root.mainloop() 
+
+
+if __name__ == '__main__':
+    main()
