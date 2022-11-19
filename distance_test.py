067446fbdc62eff1efac302b2ef51a991dfc48e9
simonsvale <s.risbjerg@stofanet.dk>
2022-11-19 23:17:23 +0100

    Put all variables into functions.

--- distance_test.py
+++ distance_test.py
@@ -1,44 +1,54 @@
 
 from math import log10
-import network
+import network, time
 
-test_amount = 30
-wifi_name = 'Bear'
 
-wifi_avg_list = []
+def network_scan(scan_amount = 60, wifi_name = 'Sim iPhone', time_between_network_scans = 0.5):
+    channel_received = False
+    wifi_avg_list = []
+    channel = []
 
-def network_scan():
-    for scans in range(test_amount):
-        wlan = network.WLAN()
+    wlan = network.WLAN()
+    wlan.active(True)
+
+    while channel_received == False:
+        networks = wlan.scan()
+        for w in networks:
+            if w[0].decode() == wifi_name:
+                channel.append(w[2])
+                channel_received = True
+
+    while True:
         networks = wlan.scan()
 
         for w in networks:
             if w[0].decode() == wifi_name:
                 wifi_avg_list.append(w[3])
-                print(w[3])
-            else:
-                print('Network was not found')
+                channel.append(w[2])
+        
+        time.sleep(time_between_network_scans)
 
-    if len(wifi_avg_list) >= 2: # normally 5     
-        avg_dBm = sum(wifi_avg_list)/len(wifi_avg_list)
-        return avg_dBm
-    else:
-        return None
+        if len(wifi_avg_list) >= scan_amount: 
+            avg_dBm = sum(wifi_avg_list)/len(wifi_avg_list)
+            return [channel, avg_dBm, wifi_avg_list, wifi_name]
 
-def cal_distance(dBm):
+def cal_distance(wifi_info):
+    environmental_constant = 24.33
 
-    if dBm != None:
-        MHz = 2400
+    if wifi_info[1] != None:
+        wifi_frequency = 2400+5*wifi_info[0][0]
         FSPL = 27.55
 
-        m = 10 ** (( FSPL - (20 * log10(MHz)) + abs(dBm) ) / 20 )
+        m = 10 ** (( FSPL - (environmental_constant * log10(wifi_frequency)) + abs(wifi_info[1]) ) / 20 )
         m=round(m,2)
         
-        print(f'wifi name = {wifi_name}')
-        print(f'Collected RSSI data: {wifi_avg_list}')
-        print(f'Average RSSI = {dBm}\n Distance to access point = {m}m\n amount of succeded tests: {len(wifi_avg_list)}/{test_amount} \n afvigelse = {abs(min(wifi_avg_list))-abs(max(wifi_avg_list))}')
-    else:
-        print('To few connections <= 5')
+        # Debug
+        print(f'wifi name = {wifi_info[3]}, channel = {wifi_info[0][0]}')
+        print(f'Collected RSSI data: {wifi_info[2]}')
+        print(f'Average RSSI = {wifi_info[1]}\nDistance to access point = {m}m\nmin = {min(wifi_info[2])}, max = {max(wifi_info[2])}')
 
-cal_distance(network_scan())
+        return m
+
+(cal_distance(network_scan(scan_amount = 5000, wifi_name = 'Sim iPhone')))
+
 
