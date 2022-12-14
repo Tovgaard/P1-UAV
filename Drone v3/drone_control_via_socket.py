013b461bb56e390366cd5acd86eec560d253b044
simonsvale <s.risbjerg@stofanet.dk>
2022-12-14 10:53:11 +0100

    Forgot a file

--- Drone v3/drone_control_via_socket.py
+++ Drone v3/drone_control_via_socket.py
@@ -1,4 +1,4 @@
-import socket, time, numpy as np
+import socket, time
 
 # Assign ip and port to client and server
 # Client
@@ -15,30 +15,26 @@ socket.bind((local_ip, local_port))
 
 tello_address = (tello_ip, tello_port)
 
-# Initialize the Tello drone.
-socket.sendto(b'command', tello_address)
-print('sent: command')
-
 try:
-    data, server = socket.recvfrom(1531)
-    data_extract = data.decode(encoding="utf-8")
-    print(np.array(socket.recvfrom(1531)))
-except Exception:
-    print('Virkede ikke')
+    # Initialize the Tello drone.
+    socket.sendto(b'command', tello_address)
+    print('sent: command')
+except Exception as i:
+    print(i)
 
 # Give the wifi a ssid (service set identifier) and a password.
 # socket.sendto(b'wifi gruppe-153 pass=trold32', tello_address)
 
-"""
 # Send command 'takeoff' to Tello drone
 socket.sendto(b'takeoff', tello_address)
 print('sent: takeoff')
 time.sleep(5)
 
-# Send command 'land' to Tello drone
-socket.sendto(b'land', tello_address)
-print('sent: landing')
-"""
+while True:
+    # Send command 'land' to Tello drone
+    socket.sendto(b'land', tello_address)
+    print('sent: landing')
+    time.sleep(2)
 
 # Close socket connection to prevent the server from listing for the client indefinitely.
 socket.close
